9b83abd6ef9f2ab15096cb27a56bcec98fd783e2
Shodiq Daffa <shodiqdaffa2302@gmail.com>
2016-10-12 18:26:55 +0700

    versi 0.1

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Navbar</title>
+	<link rel="stylesheet" href="style/bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" href="style/style.css">
+	<script src="style/jquery.min.js"></script>
+	<script src="style/bootstrap/js/bootstrap.js"></script>
+</head>
+<body>
+	<div class="container">
+	<div class="row">
+		<div class="col-xs-12 col-md-12">
+			<a href="#">
+				<img src="style/img/header.jpg" alt="" class="full">
+			</a>
+		</div>
+	</div>
+		<nav class="navbar navbar-inverse">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+						<span class="sr-only">Toggle Responsive</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<div class="navbar-brand">Menu</div>
+				</div>
+				<div class="collapse navbar-collapse navbar-justified" id="navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#"><span class="glyphicon glyphicon-home"></span></a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Profile <span class="glyphicon glyphicon-chevron-down"> </span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Sambutan Kepsek</a></li>
+								<li><a href="#">Profil Kepala Sekolah</a></li>
+								<li><a href="#">Visi & Misi</a></li>
+								<li><a href="#">Sejarah</a></li>
+								<li><a href="#">Daftar Pengajar</a></li>
+								<li><a href="#">Kontak</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Informasi <span class="glyphicon glyphicon-chevron-down"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Berita</a></li>
+								<li><a href="#">PPDB</a></li>
+								<li><a href="#">Pengumuman</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Ekstrakurikuler <span class="glyphicon glyphicon-chevron-down"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Paskibra</a></li>
+								<li><a href="#">Pramuka</a></li>
+								<li><a href="#">PMR</a></li>
+								<li><a href="#">Basketball</a></li>
+								<li><a href="#">Futsal</a></li>
+								<li><a href="#">Merpati Putih</a></li>
+								<li><a href="#">Karate</a></li>
+								<li><a href="#">Taekwondo</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="#" data-toggle="dropdown" class="dropdown-toggle">Mading <span class="glyphicon glyphicon-chevron-down"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Puisi</a></li>
+								<li><a href="#">Cerpen</a></li>
+								<li><a href="#">Pantun</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Galery</a></li>
+					</ul>
+					</ul>
+					<form class="navbar-form navbar-right">
+						<input type="text" class="form-control" placeholder="Search"><span></span>
+						<button type="submit" class="btn btn-info"><span class="glyphicon glyphicon-search"></span></button>
+					</form>
+				</div>
+		</nav>
+		</div>
+</body>
+</html>
