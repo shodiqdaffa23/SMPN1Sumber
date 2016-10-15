f86e2cbea73010f4a760c526d4cb5b061ea3ac1d
Shodiq Daffa <shodiqdaffa2302@gmail.com>
2016-10-15 19:55:43 +0700

    versi 0.1.1.5.2

--- assets/footer.php
+++ assets/footer.php
@@ -1,8 +1,16 @@
 	<div class="container">
+		<div class="content-footer">
+				<h4 class="title-footer">Kontak Sekolah</h4>
+				<ul>
+					<li><b>Alamat : </b>Jl. Rd. Dewi Sartika No. 153 Sumber - Kab. Cirebon 45611</li>
+					<li><b>Telp : </b>0231 - 321028</li>
+					<li><b>E-Mail : </b>smpn1sumber_153@yahoo.com</li>
+				</ul>
+			</div>	
 		<footer class="footer">
-				<div class="text-footer">
-					Copyright 2016
-				</div>
+			<div class="text-footer">
+				Copyright 2016
+			</div>
 		</footer>
 	</div>
 </body>
