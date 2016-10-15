90a26920e8f8546bea101fc10a88c75d8c285438
Shodiq Daffa <shodiqdaffa2302@gmail.com>
2016-10-15 11:16:17 +0700

    versi 0.1.1.5

--- index.php
+++ index.php
@@ -1,56 +1,75 @@
 <?php require"assets/header.php" ?>
 	<div class="container">
-		<div class="content">
-			<div class="container-fluid">
-				<div class="col-md-3">
-					<h4 class="title"><span><b>Login</b></span></h4>
-					<div class="login clear">	
-						<form>
-							<div class="form-group">
-								<label for="username">Username</label>
-								<input class="form-control" type="text" id="username">
+		<!-- <div class="content"> -->
+			<!-- <div class="container-fluid"> -->
+				<div class="sideleft col-md-3">
+					<div class="panel">
+						<div class="panel-body">
+							<h4 class="title"><span><b>Login</b></span></h4>
+							<div class="clear">	
+								<form>
+									<div class="form-group">
+										<label for="username">Username</label>
+										<input class="form-control" type="text" id="username">
+									</div>
+									<div class="form-group">
+										<label for="password">Password</label>
+										<input type="passwod" id="password" class="form-control">
+									</div>
+									<div class="form-group pull-right">
+										<button class="btn btn-info">Login</button>
+										<button class="btn btn-info">Sign Up</button>
+									</div>
+								</form>
 							</div>
-							<div class="form-group">
-								<label for="password">Password</label>
-								<input type="passwod" id="password" class="form-control">
-							</div>
-							<div class="form-group pull-right">
-								<button class="btn btn-info">Login</button>
-								<button class="btn btn-info">Sign Up</button>
-							</div>
-						</form>
-					</div>	
-					<h4 class="title"><span><b>Salam Pembuka</b></span></h4>
-				</div>
-				<div class="col-md-6">
-					<h4 class="title"><span><b>News</b></span></h4>
-					<article>
-						<div class="clear post">
-							<h4 class="post-title">Upacara Pembukaan</h4>
-							<img src="style/img/no-image.jpg">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium perferendis quaerat iusto quibusdam optio molestiae corporis quisquam rerum fuga iste incidunt hic numquam doloremque repellat ab ipsam voluptas animi neque, praesentium ipsa sapiente ea, voluptatem deserunt. Ipsum quas aliquid ab. <a href="">Lihat Selengkapnya ></a></p>
 						</div>
-						<hr>
-						<div class="clear post">
-							<h4 class="post-title">Upacara Pembukaan</h4>
-							<img src="style/img/no-image.jpg">
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo porro beatae debitis delectus, eius non ea repellendus earum. Provident, reiciendis, expedita. Esse molestiae rerum assumenda voluptatum consectetur perferendis saepe illum adipisci ipsam modi eos temporibus deserunt veniam quaerat ab odit impedit doloremque ad tenetur velit provident, porro praesentium. Quas, maxime. <a href="">Lihat Selengkapnya ></a></p>
-						</div>
-					</article>
-				</div>
-				<div class="col-md-3">
-					<h4 class="title"><span><b>Recent Post</b></span></h4>
-					<div class="recent-post">
-						<ul>
-							<li><a href="#">Upacara Pembukaan</a></li>
-							<li><a href="#">Upacara Pembukaan</a></li>
-							<li><a href="#">Upacara Pembukaan</a></li>
-							<li><a href="#">Upacara Pembukaan</a></li>
-						</ul>
 					</div>
-					<h4 class="title"><span><b>Recent Comment</b></span></h4>
+					<div class="panel">
+						<div class="panel-body">
+							<h4 class="title"><span><b>Salam Pembuka</b></span></h4>
+						</div>
+					</div>	
+				</div>
+				<div class="main col-md-6">
+					<div class="panel">
+						<div class="panel-body">
+							<h4 class="title"><span><b>News</b></span></h4>
+							<article>
+								<div class="clear post panel">
+									<img src="style/img/no-image.jpg">
+									<h4 class="post-title">Upacara Pembukaan</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium perferendis quaerat iusto quibusdam optio molestiae corporis quisquam rerum fuga iste incidunt hic numquam doloremque repellat ab ipsam voluptas animi neque, praesentium ipsa sapiente ea, voluptatem deserunt. Ipsum quas aliquid ab. <a href="">Lihat Selengkapnya ></a></p>
+								</div>
+								<div class="clear post panel">
+									<img src="style/img/no-image.jpg">
+									<h4 class="post-title">Upacara Pembukaan</h4>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo porro beatae debitis delectus, eius non ea repellendus earum. Provident, reiciendis, expedita. Esse molestiae rerum assumenda voluptatum consectetur perferendis saepe illum adipisci ipsam modi eos temporibus deserunt veniam quaerat ab odit impedit doloremque ad tenetur velit provident, porro praesentium. Quas, maxime. <a href="">Lihat Selengkapnya ></a></p>
+								</div>
+							</article>
+						</div>
+					</div>
+				</div>
+				<div  class="sideright col-md-3">
+					<div class="panel">
+						<div class="panel-body">
+							<h4 class="title"><span><b>Recent Post</b></span></h4>
+							<div class="recent-post">
+								<ul>
+									<li><a href="#">Upacara Pembukaan</a></li>
+									<li><a href="#">Upacara Pembukaan</a></li>
+									<li><a href="#">Upacara Pembukaan</a></li>
+									<li><a href="#">Upacara Pembukaan</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="panel">
+						<div class="panel-body">
+							<h4 class="title"><span><b>Recent Comment</b></span></h4>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
+		<!-- </div> -->
 	</div>
 <?php require"assets/footer.php" ?>
